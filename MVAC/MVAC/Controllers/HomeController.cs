f7a42a03d78b4ffc191399eac00c2f021b17e11e
Kiran Hameed/Business Intelligence/Information Technology/Islamabad <Kiran.Hameed@ptclgroup.com>
2024-02-13 22:41:52 +0500

    tourist information guiding app

---
+++ MVAC/MVAC/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PayPal.Api;
+
+namespace MVAC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult TestLayout()
+        {
+            return View();
+        }
+
+        public ActionResult OnlinePayment()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult PayPalRegistration()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult PayPalYearly()
+        {
+            return View();
+        }
+
+        public ActionResult PayPalYearlyandRegsitration()
+        {
+            return View();
+        }
+
+
+
+        public ActionResult TestingShared()
+        {
+            return View();
+        }
+
+
+    }
+
+
+}
---
+++ MVAC/MVAC/Models/StripeChargeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Stripe;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVAC.Models
+{
+    public class StripeChargeViewModel
+    {
+        public string ChargeId  { get; set; }
+
+    }
+}
